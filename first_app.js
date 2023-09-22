311acc48c5b713bf46353d7e52451444faacb5f6
hrd05 <harshdunkhwal55@gmail.com>
2023-09-22 17:57:27 +0530

    request & response

--- first_app.js
+++ first_app.js
@@ -3,8 +3,24 @@ const http = require('http');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
 
-    res.write('Harsh Tanwar')
-    res.end()
+    // console.log(req.url)
+    const url = req.url;
+    res.setHeader('Content-Type','text/html');
+    let message = '';
+    if(url === '/home'){
+        message = 'Welcome Home';
+    }
+    if(url === '/about'){
+        message = 'Welcome to About Us page';
+    }
+    if(url === '/node'){
+        message = 'Welcome to Node js Project';
+    }
+
+    res.write(`<html><body><h1>${message}</h1></body></html>`);
+    res.end();
+    
+
 });
 
 
