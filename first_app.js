0c60a3594bf7b665e4d881d31115301d95eff2a3
hrd05 <harshdunkhwal55@gmail.com>
2023-09-22 15:43:19 +0530

    creating server2

--- first_app.js
+++ first_app.js
@@ -2,15 +2,10 @@ const http = require('http');
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+
+    res.write('Harsh Tanwar')
+    res.end()
 });
 
-// Define the port to listen on
-const port = 4000;
 
-// Start the server and listen on the specified port
-server.listen(port, () => {
-  console.log('Server is running on port ' + port);
-  console.log('My name is harsh tanwar.');
-});
+server.listen(4000)
