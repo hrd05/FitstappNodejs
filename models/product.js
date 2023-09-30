f645970c59490e280c1e4ccb7af789968bebcb4b
hrd05 <harshdunkhwal55@gmail.com>
2023-09-30 20:32:52 +0530

    added delete functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,9 @@
 //storing data in a file 
+const fs = require('fs');       
 const path = require('path');
-const fs = require('fs');
+
+
+const Cart = require('./cart');
 
 const p = path.join(
     path.dirname(process.mainModule.filename),
@@ -22,7 +25,8 @@ const getProductFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imgageUrl = imageUrl;
         this.description = description;
@@ -31,13 +35,36 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
         getProductFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
+            if (this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });
+            }
+            else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            }
+
         });
+    }
+
+    static deleteById(id) {
+        getProductFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                if(!err){
+                    Cart.deleteProduct(id, product.price);
+                }
+            })
+        })
     }
 
     static fetchAll(cb) {
@@ -45,10 +72,9 @@ module.exports = class Product {
     }
 
     static findById(id, cb) {
-        getProductFromFile( products => {
+        getProductFromFile(products => {
             const product = products.find(p => p.id === id)
             cb(product);
-
         })
     }
 }
