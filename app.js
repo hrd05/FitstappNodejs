a180886ea53da9642648102e0990e72e449910f3
hrd05 <harshdunkhwal55@gmail.com>
2023-10-01 22:04:49 +0530

    starting with MySQL

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
-const ejs = require('ejs');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -17,6 +16,8 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+db.execute('SELECT * FROM products').then()
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
